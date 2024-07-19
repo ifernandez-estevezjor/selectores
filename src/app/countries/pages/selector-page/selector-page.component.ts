c54bdbde97fa55475a5392b03f2c4a3d6ebedceb
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-18 18:02:27 -0600

    Seccion 19: Selectores Multiples - Mostrar fronteras de los paises

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'countries-selector-page',
@@ -17,17 +17,19 @@ export class SelectorPageComponent implements OnInit{
   ){}
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: SmallCountry[] = [];
   
   //Conectar campos con el formulario
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   })
   
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
   
   //Obtener las regiones para cargarlas en el formulario
@@ -38,10 +40,24 @@ export class SelectorPageComponent implements OnInit{
   onRegionChanged(): void{
     this.myForm.get('region')!.valueChanges
     .pipe(
+      tap( () => this.myForm.get('country')!.setValue('')),
       switchMap(region => this.countriesService.getCountriesByRegion(region)),
     )
     .subscribe(countries => {
       this.countriesByRegion = countries;
+    });
+  }
+
+  onCountryChanged(): void{
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap( () => this.myForm.get('border')!.setValue('')),
+      filter((value: string) => value.length > 0),
+      switchMap( (alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode)),
+      switchMap( (country) => this.countriesService.getCountryBordersByCodes(country.borders))
+    )
+    .subscribe(countries => {
+      this.borders = countries;
     });
   }
 
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, of, tap } from 'rxjs';
+import { combineLatest, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -36,4 +36,33 @@ export class CountriesService {
       //tap(response => console.log({response}))
     )
   }
+
+  //obtener los borders por pais/region
+  getCountryByAlphaCode(alphaCode: string): Observable<SmallCountry>{
+    console.log({alphaCode})
+
+    const url = `${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url)
+      .pipe(
+        map(country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+  }
+
+  getCountryBordersByCodes(borders: string[]): Observable<SmallCountry[]>{
+    if(!borders || borders.length === 0) return of([]);
+
+    const countriesRequests: Observable<SmallCountry>[] = [];
+
+    borders.forEach(code => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+    return combineLatest(countriesRequests);
+  }
+
+
 }
